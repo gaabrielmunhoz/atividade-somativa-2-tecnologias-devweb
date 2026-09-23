48dd1b0a736a783ce859901c541794f373f2ce46
Gabriel Munhoz <gabrielmunhoz21@gmail.com>
2026-09-23 18:00:09 -0300

    cria a página que puxa os dados do usuário

--- src/paginas/Principal.jsx
+++ src/paginas/Principal.jsx
@@ -1,9 +1,73 @@
-
+import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { onAuthStateChanged, signOut } from "firebase/auth"
+import { doc, getDoc } from "firebase/firestore"
+import { auth, db } from "../firebase"
 
 function Principal(){
+    const [usuario, setUsuario] = useState(null)
+    const [mensagem, setMensagem] = useState("")
+    const [carregando, setCarregando] = useState(true)
+
+    const navigate = useNavigate()
+
+    useEffect(()=> {
+        const verificarUsuario= onAuthStateChanged(auth, async (usuarioAutenticado)=> {
+            if (!usuarioAutenticado){
+                navigate("/login")
+                return
+            }
+
+            try {
+                const referenciaUsuario = doc(
+                    db,
+                    "usuarios",
+                    usuarioAutenticado.uid
+                )
+
+                const dadosUsuario = await getDoc(referenciaUsuario)
+
+                if (dadosUsuario.exists()) {
+                    setUsuario(dadosUsuario.data())
+                } else {
+                    setMensagem("Dados não encontrados.")
+                }
+            } catch (erro){
+                console.log(erro)
+                setMensagem("Não foi possível carregar os dados do usuário.")
+            } finally {
+                setCarregando(false)
+            }
+        })
+
+        return ()=> verificarUsuario()
+    }, [navigate])
+
+    if (carregando){
+        return <p>Carregando...</p>
+    }
+
+    async function sair(){
+            await signOut(auth)
+            setTimeout(()=> {navigate("/login")}, 1500)
+        }
+
     return(
         <div>
             <h1>Principal</h1>
+
+            {mensagem && <p>{mensagem}</p>}
+
+            {usuario && (
+                <div>
+                    <p>Nome: {usuario.nome}</p>
+                    <p>Sobrenome: {usuario.sobrenome}</p>
+                    <p>Data de Nascimento: {usuario.dataNascimento}</p>
+                </div>
+            )}
+            <br />
+            <button type="button" onClick={sair}>Sair</button>
+
         </div>
     )
 }
