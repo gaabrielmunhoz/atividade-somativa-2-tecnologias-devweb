7850cf838b8ec2400b8ad121a989e0228fdcbf04
Gabriel Munhoz <gabrielmunhoz21@gmail.com>
2026-09-23 16:16:15 -0300

    corrige a falte de id nos inputs

--- src/paginas/Cadastro.jsx
+++ src/paginas/Cadastro.jsx
@@ -82,31 +82,31 @@ function Cadastro(){
                 <h1>Cadastro</h1>
                 <form onSubmit={salvarCadastro}>
                     <label htmlFor="nome">Nome</label>
-                    <input type="text" value={nome} onChange={(event)=> setNome(event.target.value)} placeholder="Seu nome" required />
+                    <input type="text" id="nome" value={nome} onChange={(event)=> setNome(event.target.value)} placeholder="Seu nome" required />
                     <br />
 
                     <label htmlFor="sobrenome">Sobrenome</label>
-                    <input type="text" value={sobrenome} onChange={(event) => setSobrenome(event.target.value)} placeholder="Seu sobrenome" required />
+                    <input type="text" id="sobrenome" value={sobrenome} onChange={(event) => setSobrenome(event.target.value)} placeholder="Seu sobrenome" required />
                     <br />
 
                     <label htmlFor="dataNascimento">Data de Nascimento</label>
-                    <input type="date" value={dataNascimento} onChange={(event)=> setDataNascimento(event.target.value)} placeholder="DD/MM/AAAA" required />
+                    <input type="date" id="dataNascimento" value={dataNascimento} onChange={(event)=> setDataNascimento(event.target.value)} placeholder="DD/MM/AAAA" required />
                     <br />
 
                     <label htmlFor="email">E-mail</label>
-                    <input type="email" value={email} onChange={(event)=> setEmail(event.target.value)} placeholder="seu@email.com" required />
+                    <input type="email" id="email" value={email} onChange={(event)=> setEmail(event.target.value)} placeholder="seu@email.com" required />
                     <br />
 
                     <label htmlFor="confirmarEmail">Confirmar e-mail</label>
-                    <input type="email" value={confirmarEmail} onChange={(event)=> setConfirmarEmail(event.target.value)} placeholder="seu@email.com" required />
+                    <input type="email" id="confirmarEmail" value={confirmarEmail} onChange={(event)=> setConfirmarEmail(event.target.value)} placeholder="seu@email.com" required />
                     <br />
 
                     <label htmlFor="senha">Senha</label>
-                    <input type="password" value={senha} onChange={(event)=> setSenha(event.target.value)} placeholder="Sua senha" required />
+                    <input type="password" id="senha" value={senha} onChange={(event)=> setSenha(event.target.value)} placeholder="Sua senha" required />
                     <br />
 
                     <label htmlFor="confirmarSenha">Confirmar senha</label>
-                    <input type="password" value={confirmarSenha} onChange={(event) => setConfirmarSenha(event.target.value)} placeholder="Confirme a sua senha" required />
+                    <input type="password" id="confirmarSenha" value={confirmarSenha} onChange={(event) => setConfirmarSenha(event.target.value)} placeholder="Confirme a sua senha" required />
                     <br />
 
                     <button type="submit">Cadastrar</button>
