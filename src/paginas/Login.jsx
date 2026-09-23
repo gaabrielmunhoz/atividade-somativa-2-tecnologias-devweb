fb213e701a69152ffbf18d0c295df015f3b7be9d
Gabriel Munhoz <gabrielmunhoz21@gmail.com>
2026-09-23 16:16:38 -0300

    cria a página de login

--- src/paginas/Login.jsx
+++ src/paginas/Login.jsx
@@ -1,9 +1,63 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { auth, db } from "../firebase"
+import { signInWithEmailAndPassword } from "firebase/auth"
 
 
 function Login(){
+
+    const [email, setEmail] = useState("")
+    const [senha, setSenha] = useState("")
+    const [mensagem, setMensagem] = useState("")
+
+    const navigate = useNavigate()
+
+    const entrar = async (event) => {
+        event.preventDefault()
+        setMensagem("")
+
+        try {
+
+            const verificarAcesso = await signInWithEmailAndPassword(
+                auth,
+                email,
+                senha
+            )
+            setMensagem("Usuário logado com sucesso!", verificarAcesso.user)
+            navigate("/principal")
+
+        } catch (erro) {
+            console.log(erro)
+            if (erro.code === "auth/invalid-email") {
+                setMensagem("E-mail inválido.")
+            } else if (erro.code === "auth/user-not-found"){
+                setMensagem("Usuário não encontrado.")
+            } else if (erro.code === "auth/wrong-password") {
+                setMensagem("Senha incorreta.")
+            } else if (erro.code === "auth/invalid-credential") {
+                setMensagem("E-mail ou senha incorretos.")
+            } else {
+                setMensagem("Não foi possível realizar o login.")
+            }
+        }
+    }
+
     return(
         <div>
             <h1>Login</h1>
+            <form onSubmit={entrar}>
+                <label htmlFor="email">E-mail</label>
+                <input type="email" id="email" value={email} onChange={(event)=> setEmail(event.target.value)} placeholder="seu@email.com" required />
+                <br />
+
+                <label htmlFor="senha">Senha</label>
+                <input type="password" id="senha" value={senha} onChange={(event)=> setSenha(event.target.value)} placeholder="Sua senha" required />
+                <br />
+
+                <button type="submit">Entrar</button>
+                {mensagem && <p>{mensagem}</p>}
+
+            </form>
         </div>
     )
 }
